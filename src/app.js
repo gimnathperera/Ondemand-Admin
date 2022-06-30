dd7b6b397dac8234e7ca07b968cac60acf75de5d
Gimnath-Perera <gimnathperera.com>
2022-06-30 10:30:17 +0530

    [FIX] removed unwaned code from app.js

--- src/app.js
+++ src/app.js
@@ -61,10 +61,6 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
-// send back a 404 error for any unknown api request
-// app.use((req, res, next) => {
-//   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-// });
 
 // convert error to ApiError, if needed
 app.use(errorConverter);
