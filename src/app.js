b391e39a3b64ecf61aa9899738ae4ba9881b6717
gimnathperera <gimnathperera.com>
2022-06-30 10:37:51 +0530

    [FIX] removed unwaned code from server

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
 const passport = require('passport');
-const httpStatus = require('http-status');
 const path = require('path');
 
 const config = require('./config/config');
@@ -14,7 +13,6 @@ const { jwtStrategy } = require('./config/passport');
 const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
 const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
 
 const app = express();
 
